c3d6162fc660652e62d2ca0ed3569f0c7de4de0e
Brad Flaugher <brad@bradflaugher.com>
2023-11-15 20:56:16 -0500

    refactor start.sh

--- start.sh
+++ start.sh
@@ -15,121 +15,117 @@ echo ""
 echo "The Medusa Project is committed to delivering state-of-the-art technology"
 echo "solutions for your machine learning projects, ensuring a seamless and"
 echo "productive user experience."
+echo ""
+echo "For community support join our discord at https://discord.gg/medusaml"
 echo "================================================================================"
 echo ""
 
-echo "Do you want to run vanilla Ubuntu, PyTorch or TensorFlow? (U)buntu/(P)yTorch/(T)ensorFlow"
+echo "Select the base docker image to run (default is Ubuntu): (U)buntu/(P)yTorch/(T)ensorFlow. Press Enter for Ubuntu."
 read framework
 framework=$(echo $framework | tr '[:upper:]' '[:lower:]')
+optional_installs=""
 
-echo "Do you want to run open-interpreter? (Y)es/(N)o"
+echo "Do you want to run open-interpreter? (Y)es/(N)o. Press Enter for No (default)."
 read open_interpreter
 open_interpreter=$(echo $open_interpreter | tr '[:upper:]' '[:lower:]')
+user_setup=""
+bash=""
 
-echo "Do you have nvidia-docker installed and want to run on a GPU? (Y)es/(N)o"
+echo "Do you have nvidia-docker installed and want to run on a GPU? (Y)es/(N)o. Press Enter for No (default)."
 read use_gpu
 use_gpu=$(echo $use_gpu | tr '[:upper:]' '[:lower:]')
 
-echo "Do you want to rebuild from scratch? (--no-cache)? (Y)es/(N)o"
-read no_cache 
-no_cache=$(echo $no_cache | tr '[:upper:]' '[:lower:]')
+
+echo "Do you want to skip building the container (you MUST run this at least once)? (Y)es/(N)o. Press Enter for No (default)."
+read skip_build 
+skip_build=$(echo $skip_build | tr '[:upper:]' '[:lower:]')
+
+
+tag=""
 
 case $framework in
     p|pytorch)
-        framework="pytorch"
+        framework="pytorch/pytorch:latest"
+	optional_installs="--build-arg OPTIONAL_INSTALLS=true"
+	tag+="-pytorch"
         ;;
     t|tensorflow)
-        framework="tensorflow"
+        framework="tensorflow/tensorflow:latest-jupyter"
+	tag+="-tensorflow"
         ;;
     u|ubuntu)
-        framework="ubuntu"
+        framework="ubuntu:latest"
+	tag+="-ubuntu"
         ;;
     *)
-        echo "Invalid input for framework. Defaulting to Ubuntu."
-        framework="ubuntu"
-        ;;
+        framework="ubuntu:latest"
+        tag+="-ubuntu"
+	;;
 esac
 
 case $open_interpreter in
     y|yes)
-        open_interpreter="yes"
-        ;;
+        open_interpreter="--build-arg SETUP_ENTRYPOINT=true"
+        bashopt="bash"
+	user_setup="-e UID=$(id -u) -e GID=$(id -g)"
+	tag+="-oi"
+	;;
     n|no)
-        open_interpreter="no"
+        open_interpreter=""
+	bashopt=""
+	user_setup="-u $(id -u):$(id -g)"
         ;;
     *)
-        echo "Invalid input for open-interpreter. Defaulting to No."
-        open_interpreter="no"
+        open_interpreter=""
+	bashopt=""
+	user_setup="-u $(id -u):$(id -g)"
         ;;
 esac
 
 case $use_gpu in
     y|yes)
-        use_gpu="yes"
+        use_gpu="--gpus all"
         ;;
     n|no)
-        use_gpu="no"
+        use_gpu=""
         ;;
     *)
-        echo "Invalid input for GPU usage. Defaulting to No."
-        use_gpu="no"
+        use_gpu=""
         ;;
 esac
 
-case $no_cache in
+no_cache=""
+
+case $skip_build in
     y|yes)
-        no_cache="--no-cache"
-        ;;
+	skip_build="yes"
+	;;
     n|no)
-        no_cache=""
+        skip_build="no"
+	echo "Do you want to force a refresh (--no-cache)? (Y)es/(N)o. Press Enter for No (default)."
+	read no_cache 
+	no_cache=$(echo $no_cache | tr '[:upper:]' '[:lower:]')
+	case $no_cache in
+		y|yes)
+			no_cache="--no-cache"
+			;;
+		n|no)
+			no_cache=""
+			;;
+		*)	
+			no_cache=""
+			;;
+	esac
         ;;
     *)
-        echo "Invalid input for cache clearing, Defaulting tousing cache."
-        no_cache=""
+        skip_build="no"
         ;;
 esac
 
-if [ "$framework" = "pytorch" ]; then
-if [ "$open_interpreter" = "yes" ] && [ "$use_gpu" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg OPTIONAL_INSTALLS=true --build-arg SETUP_ENTRYPOINT=true -t mlt . $no_cache
-    sudo docker run --gpus all -e UID=$(id -u) -e GID=$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/workspace -it --rm -p 8888:8888 mlt
-elif [ "$open_interpreter" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg OPTIONAL_INSTALLS=true --build-arg SETUP_ENTRYPOINT=true -t mlt . $no_cache
-    sudo docker run -e UID=$(id -u) -e GID=$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/workspace -it --rm -p 8888:8888 mlt bash
-elif [ "$use_gpu" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg OPTIONAL_INSTALLS=true -t mlt . $no_cache
-    sudo docker run --gpus all -u $(id -u):$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/workspace -it --rm -p 8888:8888 mlt
-else
-    sudo docker build -f install/Dockerfile --build-arg OPTIONAL_INSTALLS=true -t mlt . $no_cache
-    sudo docker run -u $(id -u):$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/workspace -it --rm -p 8888:8888 mlt
+
+if [ "$skip_build" = "no" ]; then
+	sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=$framework $optional_installs $open_interpreter -t mlt$tag . $no_cache
 fi
-elif [ "$framework" = "tensorflow" ]; then
-if [ "$open_interpreter" = "yes" ] && [ "$use_gpu" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=tensorflow/tensorflow:latest-jupyter --build-arg SETUP_ENTRYPOINT=true  -t mlt . $no_cache
-    sudo docker run --gpus all -e UID=$(id -u) -e GID=$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt bash
-elif [ "$open_interpreter" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=tensorflow/tensorflow:latest-jupyter --build-arg SETUP_ENTRYPOINT=true -t mlt . $no_cache
-    sudo docker run -e UID=$(id -u) -e GID=$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt bash
-elif [ "$use_gpu" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=tensorflow/tensorflow:latest-jupyter -t mlt . $no_cache
-    sudo docker run --gpus all -u $(id -u):$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt
-else
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=tensorflow/tensorflow:latest-jupyter -t mlt . $no_cache
-    sudo docker run -u $(id -u):$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt
-fi
-elif [ "$framework" = "ubuntu" ]; then
-if [ "$open_interpreter" = "yes" ] && [ "$use_gpu" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=ubuntu:latest --build-arg SETUP_ENTRYPOINT=true -t mlt . $no_cache
-    sudo docker run --gpus all -e UID=$(id -u) -e GID=$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt bash
-elif [ "$open_interpreter" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=ubuntu:latest --build-arg SETUP_ENTRYPOINT=true -t mlt . $no_cache
-    sudo docker run -e UID=$(id -u) -e GID=$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt bash
-elif [ "$use_gpu" = "yes" ]; then
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=ubuntu:latest -t mlt . $no_cache
-    sudo docker run --gpus all -u $(id -u):$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt
-else
-    sudo docker build -f install/Dockerfile --build-arg BASE_IMAGE=ubuntu:latest -t mlt . $no_cache
-    sudo docker run -u $(id -u):$(id -g) -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/tf -it --rm -p 8888:8888 mlt
-fi
-fi
+
+sudo docker run $use_gpu $user_setup -e OPENAI_API_KEY=$OPENAI_API_KEY -v $(pwd):/workspace -it --rm -p 8888:8888 mlt$tag $bashopt
 
